0c4feca111697ef57873b3d2371f27a0a814d8fe
Vinicius Damião <vinidamiaop@gmail.com>
2021-06-05 10:26:47 -0300

    Started to implement redux

---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+
+const contador = () => 0;
+
+const reducer = combineReducers({ contador });
+const middleware = [...getDefaultMiddleware()];
+
+const store = configureStore({ reducer, middleware });
+
+export default store;
