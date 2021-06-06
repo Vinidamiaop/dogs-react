01c6ddb747c39c0390378caa082c2000d04198ae
Vinicius Damião <vinidamiaop@gmail.com>
2021-06-06 11:25:11 -0300

    Added user auto login to user reducer

--- src/store/user.js
+++ src/store/user.js
@@ -1,6 +1,6 @@
 import { USER_GET } from "../api";
 import createAsyncSlice from "./Helper/createAsyncSlice";
-import { fetchToken } from "./token";
+import { fetchToken, resetTokenState } from "./token";
 
 const slice = createAsyncSlice({
   name: "user",
@@ -9,11 +9,28 @@ const slice = createAsyncSlice({
 
 export const fetchUser = slice.asyncAction;
 
+const { resetState: resetUserState, fetchError } = slice.actions;
+
 export const userLogin = (user) => async (dispatch) => {
   const { payload } = await dispatch(fetchToken(user));
-  console.log(payload);
+
   if (payload.token) {
     await dispatch(fetchUser(payload.token));
+    window.localStorage.setItem("token", payload.token);
+  }
+};
+
+export const userLogout = () => async (dispatch) => {
+  dispatch(resetUserState());
+  dispatch(resetTokenState());
+  window.localStorage.removeItem("token");
+};
+
+export const autoLogin = () => async (dispatch, getState) => {
+  const { token } = getState();
+  if (token?.data?.token) {
+    const { type } = await dispatch(fetchUser(token.data.token));
+    if (type === fetchError.type) dispatch(userLogout());
   }
 };
 
